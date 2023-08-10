0e9c61fb455bc79f4f3315756f9cbba1e9cc325f
Emotions <estskywalker@gmail.com>
2023-08-11 01:30:23 +0700

    fix(server) : add facade design pattern

---
+++ src/serverwofacade/ServerFacade.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serverwofacade;
+
+/**
+ *
+ * @author emotions
+ */
+public class ServerFacade {
+
+    public static void startServer() {
+        ScheduleServer scheduleServer = new ScheduleServer();
+        scheduleServer.startBooting();
+        scheduleServer.readSystemConfigFile();
+        scheduleServer.init();
+        scheduleServer.initializeContext();
+        scheduleServer.initializeListeners();
+        scheduleServer.createSystemObjects();
+        System.out.println("Start working......");
+    }
+
+    public static void stopServer() {
+        ScheduleServer scheduleServer = new ScheduleServer();
+        System.out.println("After work done.........");
+        scheduleServer.releaseProcesses();
+        scheduleServer.destory();
+        scheduleServer.destroySystemObjects();
+        scheduleServer.destoryListeners();
+        scheduleServer.destoryContext();
+        scheduleServer.shutdown();
+    }
+}
--- src/serverwofacade/ServerWOFacade.java
+++ src/serverwofacade/ServerWOFacade.java
@@ -3,25 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package serverwofacade;
+
 public class ServerWOFacade {
+
     public static void main(String[] args) {
-        ScheduleServer scheduleServer = new ScheduleServer();
-	scheduleServer.startBooting();
-	scheduleServer.readSystemConfigFile();
-	scheduleServer.init();
-	scheduleServer.initializeContext();
-	scheduleServer.initializeListeners();
-	scheduleServer.createSystemObjects();
-	System.out.println("Start working......");
-	System.out.println("After work done.........");
-	scheduleServer.releaseProcesses();
-	scheduleServer.destory();
-	scheduleServer.destroySystemObjects();
-	scheduleServer.destoryListeners();
-	scheduleServer.destoryContext();
-	scheduleServer.shutdown();
+        ServerFacade.startServer();
+        ServerFacade.stopServer();
     }
-    
+
 }
